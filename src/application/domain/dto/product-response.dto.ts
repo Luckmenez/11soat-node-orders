93eb2eda2d2b08bb71f23d7df8a0696d61ac10fa
Lucas Menezes <luckmenez@gmail.com>
2025-11-25 21:46:32 -0300

    refactor: adding product return response

--- src/application/domain/dto/product-response.dto.ts
+++ src/application/domain/dto/product-response.dto.ts
@@ -1,30 +1,3 @@
-interface ProductEntity {
-  name: string;
-  description?: string | null;
-  price: number;
-  available: boolean;
-  url_img?: string | null;
+export interface ProductValidateResponse {
+  all_available: boolean;
 }
-
-interface Category {
-  id: number | null;
-  name: string;
-  description: string | null;
-  createdAt?: Date;
-  updatedAt?: Date;
-}
-
-interface Product {
-  id: number;
-  name: string;
-  description: string | null;
-  amount: number;
-  url_img: string;
-  customizable: boolean;
-  available: boolean;
-  category_id: number | null;
-  category?: Category;
-  productItems?: ProductEntity[];
-}
-
-export type ProductResponseDto = Product;
