c5833397d8204a03b683f9593e19a749b0cd6e9e
Lucas Menezes <luckmenez@gmail.com>
2025-12-15 17:45:45 -0300

    feat: adicionado token como entrada de criação de order

--- src/application/ports/input/order.use-case.port.ts
+++ src/application/ports/input/order.use-case.port.ts
@@ -2,5 +2,5 @@ import { CreateOrderDto } from 'src/application/domain/dto/order.db.interface';
 import { OrderEntity } from 'src/application/domain/entities/orders.entity';
 
 export interface CreateOrderUseCasePort {
-  execute(data: CreateOrderDto): Promise<OrderEntity>;
+  execute(data: CreateOrderDto, token: string): Promise<OrderEntity>;
 }
