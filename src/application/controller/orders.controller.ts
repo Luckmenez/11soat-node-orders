fe81850f0f06b38ab7d5d6e4cac83e41aba65288
Lucas Menezes <luckmenez@gmail.com>
2025-12-31 18:37:24 -0300

    feat: adicionado controller get-paginated

--- src/application/controller/orders.controller.ts
+++ src/application/controller/orders.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Inject, Post, Headers } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Headers, Get } from '@nestjs/common';
 import { CreateOrderDto } from 'src/application/domain/dto/order.db.interface';
-import { CreateOrderUseCasePort } from 'src/application/ports/input/order.use-case.port';
+import { CreateOrderUseCasePort } from 'src/application/ports/input/create-order.port';
+import { GetOrdersPaginatedDto } from '../domain/dto/orders-get-paginated.dto';
+import { GetOrdersPaginatedPort } from '../ports/input/get-orders-paginated.port';
 
 @Controller('orders')
 export class OrdersController {
   constructor(
     @Inject('CreateOrderUseCasePort')
     private readonly createOrderUseCase: CreateOrderUseCasePort,
+    @Inject('GetOrdersPaginatedUseCasePort')
+    private readonly getOrdersPaginatedUseCase: GetOrdersPaginatedPort,
   ) {}
 
   @Post('create-order')
@@ -15,5 +19,13 @@ export class OrdersController {
     @Headers('Authorization') token: string,
   ) {
     return this.createOrderUseCase.execute(createOrderDto, token);
+  }
+
+  @Get('get-paginated')
+  getOrdersPaginated(@Body() getOrdersPaginatedDto: GetOrdersPaginatedDto) {
+    return this.getOrdersPaginatedUseCase.execute({
+      page: getOrdersPaginatedDto.page,
+      limit: getOrdersPaginatedDto.limit,
+    });
   }
 }
