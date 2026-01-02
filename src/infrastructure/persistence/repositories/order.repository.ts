f2bb545ba5d9109d3c76f39c02d422a773e1e7fc
Lucas Menezes <luckmenez@gmail.com>
2026-01-02 17:56:04 -0300

    feat: adicionado função de update status

--- src/infrastructure/persistence/repositories/order.repository.ts
+++ src/infrastructure/persistence/repositories/order.repository.ts
@@ -59,11 +59,24 @@ export class PrismaOrderRepository implements OrderRepositoryPort {
     return order ? OrderMapper.toEntity(order) : null;
   }
 
-  async updateStatus(id: number, status: OrderStatus): Promise<void> {
-    await this.prisma.order.update({
+  async updateStatus(
+    id: number,
+    status: OrderStatus,
+    transactionId: string,
+  ): Promise<OrderEntity> {
+    const result = await this.prisma.order.update({
       where: { id },
-      data: { status },
+      data: { status, transactionId },
+      include: {
+        items: {
+          include: {
+            customerItems: true,
+          },
+        },
+      },
     });
+
+    return OrderMapper.toEntity(result);
   }
 
   async getPaginatedOrders(
