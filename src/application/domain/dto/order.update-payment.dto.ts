282563778451db0dc3b21d68a69810b96760899a
Lucas Menezes <luckmenez@gmail.com>
2026-01-03 13:55:17 -0300

    fix: removido header de chamada

--- src/application/domain/dto/order.update-payment.dto.ts
+++ src/application/domain/dto/order.update-payment.dto.ts
@@ -1,10 +1,8 @@
 import { OrderStatus } from 'src/application/value-objects/order-status.enum';
 
-export class UpdateOrderParamPaymentDto {
-  orderId: number;
-}
-
 export class UpdateOrderPaymentDto {
+  orderId: number;
   status: OrderStatus;
   transactionId: string;
+  amount: number;
 }
